7fa2e58d30b42b470740fabc4c0bbd68825d249b
Andres <anondiaz@gmail.com>
2025-03-26 13:28:59 +0100

    empezamos

---
+++ js/carrito.js
@@ -0,0 +1,40 @@
+/*
+Hay que programar un carrito de compra de fruta.
+
+El cliente eligirá que fruta quiere haciendo click sobre la imagen.
+Un mensaje emergente le preguntará qué cantidad quiere.
+
+Esta información se mostrará a la derecha, bajo "Total carrito", 
+en <p id="carrito"></p>, de esta forma:
+ Kiwi 2 kg x 4,20€/kg = 8,40 €
+
+El total se actualizará con cada compra
+ Total Compra: 8,40€
+ 
+Se dará la opción de añadir o no más productos que se mostrarán
+a continuación de los anteriores, y se sumará todo en el total. 
+Por ejemplo:  
+ Kiwi 2 kg x 4, 20€/kg = 8, 40€
+ Pomelo 1 kg x 2,50€/kg = 2,50€
+ Total Compra: 10,90€
+
+Puedes modificar el código facilitado si ello te ayuda con el ejercicio,
+pero deberás justificarlo.
+
+Recuerda la importancia comentar con detalle el código.
+
+ Lo importante es el cálculo, no los estilos css
+ */
+
+// Se añaden id's a Imagenes y parrafos para una mejor gestión de los artículos
+
+let frutas = document.getElementsByClassName("imatges")
+
+for (i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
+
+
+//  div.addEventListener("click", () => {
+//     div.innerHTML = "<h2>Soy un h2 nuevo</h2>"
+// })
